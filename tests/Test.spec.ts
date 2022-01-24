b4eec0497847f2675084615d3e636b13f114657c
likuyao <likuyao@easyops.cn>
2022-01-24 11:43:16 +0800

    test(blog): change test

--- tests/Test.spec.ts
+++ tests/Test.spec.ts
@@ -1,10 +1,10 @@
-// import { mount } from '@vue/test-utils'
-// import Test from '../src/views/Test.vue'
+describe('sum function test', () => {
+  it('sum(1, 2) === 3', () => {
+    expect(1 + 2).toBe(3)
+  })
 
-// test('Test.vue', async () => {
-//   const wrapper = mount(Test)
-//   expect(wrapper.html()).toContain('Unit Test Page')
-//   expect(wrapper.html()).toContain('count is: 0')
-//   await wrapper.find('button').trigger('click')
-//   expect(wrapper.html()).toContain('count is: 1')
-// })
+  // 这里 test 和 it 没有明显区别，it 是指: it should xxx, test 是指 test xxx
+  // test('sum(1, 2) === 3', () => {
+  //   expect(1 + 2).toBe(3)
+  // })
+})
